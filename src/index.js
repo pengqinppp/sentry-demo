83f7d7ae5c28b04f01f91659ac7cb8e0a674ab4c
pengqin <pengqin@haier.com>
2019-06-21 17:15:54 +0800

    [Feature] init proj
    Change-Id: Iaa7e3a815738963c7b10eddb0e8568cb357c45f8

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+Sentry.init({
+  dsn: "https://e2ea021be8314648829b50b337e3f7c2@sentry.io/1487164",
+  environment: 'staging',
+  release: "my-app@1.0.0"
+});
+
+Sentry.captureException(new Error("Something broke"));
+Sentry.captureMessage('Something went wrong');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
