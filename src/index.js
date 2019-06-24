7d13d7c4d3374f2e0396c4e105680c03b926ec87
pengqinppp <p3ngqin@icloud.com>
2019-06-24 16:04:39 +0800

    [Feature] 1.0.6
    Change-Id: I3f2f16f15ba3eaa5eb895b601b686a867920ffd7

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ ReactDOM.render(<App />, document.getElementById('root'));
 Sentry.init({
   dsn: "https://e2ea021be8314648829b50b337e3f7c2@sentry.io/1487164",
   environment: 'production',
-  release: "1.0.5"
+  release: "1.0.6"
 });
 
-Sentry.captureException(new Error("error 1.0.5 "));
+Sentry.captureException(new Error("error 1.0.6 "));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
